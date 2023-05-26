8a160a678c30e1cd68f23cb691ff67e41e4407e0
Foxiliant <56639594+Foxiliant@users.noreply.github.com>
2023-05-26 14:20:58 +0300

    0.0.6

--- script.js
+++ script.js
@@ -17,7 +17,10 @@ window.addEventListener('scroll', function() {
     if (scrollTop === 0) {
         header.style.backgroundColor = 'rgba(255, 255, 255, 0)';
         contentSections.forEach(function(section) {
-            section.style.opacity = '1'; // Восстановление прозрачности текста
+            var rect = section.getBoundingClientRect();
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                section.style.opacity = '1';
+            }
         });
     } else {
         header.style.backgroundColor = 'rgba(255, 255, 255, 1)';
