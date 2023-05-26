f25ef152bdf78d7867bca16ddfd48b356e5fe6d0
Foxiliant <56639594+Foxiliant@users.noreply.github.com>
2023-05-26 14:02:09 +0300

    0.0.4/1

--- script.js
+++ script.js
@@ -12,10 +12,14 @@ window.addEventListener('scroll', function() {
 window.addEventListener('scroll', function() {
     var header = document.getElementById('header');
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var contentSections = document.querySelectorAll('.content-section');
 
     if (scrollTop === 0) {
-        header.style.backgroundColor = 'rgba(255, 255, 255, 0)'; /* Прозрачный фон */
+        header.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+        contentSections.forEach(function(section) {
+            section.style.opacity = '1'; // Восстановление прозрачности текста
+        });
     } else {
-        header.style.backgroundColor = 'rgba(255, 255, 255, 1)'; /* Непрозрачный фон */
+        header.style.backgroundColor = 'rgba(255, 255, 255, 1)';
     }
 });
