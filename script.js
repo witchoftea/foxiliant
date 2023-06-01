84c8469ae0dc3f726953e9c5ce355eae0c910974
witchoftea <arseniy.koshl@yahoo.com>
2023-06-01 18:12:07 +0300

    0.0.7

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 window.addEventListener('scroll', function() {
-    var underbody = document.getElementById('underbody');
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (scrollTop > 0) {
-        underbody.style.display = 'block';
-    } else {
-        underbody.style.display = 'none';
-    }
-});
-
-window.addEventListener('scroll', function() {
     var header = document.getElementById('header');
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     var contentSections = document.querySelectorAll('.content-section');
