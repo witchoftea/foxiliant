3369f438dc9dd8b074a82aabc795e7bd429e40bc
witchoftea <arseniy.koshl@yahoo.com>
2023-06-01 19:13:00 +0300

    0.0.9

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var header = document.getElementById('header');
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     var contentSections = document.querySelectorAll('.content-section');
 
     if (scrollTop === 0) {
-        header.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-        contentSections.forEach(function(section) {
+        header.style.backgroundColor = 'rgba(60, 60, 60, 1)';
+        contentSections.forEach(function (section) {
             var rect = section.getBoundingClientRect();
             if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
                 section.style.opacity = '1';
